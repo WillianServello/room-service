cf026efad864efe3c256f562ebd4536ba253c430
WillianServello <rafaelwillian299@gmail.com>
2026-06-07 03:01:05 -0300

    new repository: working on WPF

---
+++ roomservice.presentation/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace roomservice.presentation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
