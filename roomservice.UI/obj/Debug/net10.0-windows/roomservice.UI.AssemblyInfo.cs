d99cfbac2c1f5b6ffb61d0350c03ff90e174a3e4
WillianServello <rafaelwillian299@gmail.com>
2026-06-09 11:37:46 -0300

    feat: added common resource

--- roomservice.UI/obj/Debug/net10.0-windows/roomservice.UI.AssemblyInfo.cs
+++ roomservice.UI/obj/Debug/net10.0-windows/roomservice.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("roomservice.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf026efad864efe3c256f562ebd4536ba253c430")]
 [assembly: System.Reflection.AssemblyProductAttribute("roomservice.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("roomservice.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
